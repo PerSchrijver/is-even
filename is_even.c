9f189ecba8662864953dc0bc762696d4b62248d2
PerSchrijver <perschrijver@gmail.com>
2024-06-07 12:01:16 +0200

    Another notch in the even belt: 2542.

--- is_even.c
+++ is_even.c
@@ -10172,6 +10172,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2542)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

7b0ef2ea511f6a0ce59191fe4e364401facff7d2
PerSchrijver <perschrijver@gmail.com>
2024-06-07 12:00:48 +0200

    Handing 2514 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -10060,6 +10060,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2514)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

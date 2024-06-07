d23af08ac656598587a6f80e6372f3a14a078561
PerSchrijver <perschrijver@gmail.com>
2024-06-07 12:01:13 +0200

    One small step for 2539, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -10160,6 +10160,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2539)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

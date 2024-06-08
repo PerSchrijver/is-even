ea71920c91c95595a0825ef30cae70c2935210dd
PerSchrijver <perschrijver@gmail.com>
2024-06-08 12:00:09 +0200

    Adding 2561 so the odd numbers stop whining.

--- is_even.c
+++ is_even.c
@@ -10248,6 +10248,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2561)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

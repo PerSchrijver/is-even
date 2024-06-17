a0047808d8d0d1752e7dfeac79ac20490f6ea69b
PerSchrijver <perschrijver@gmail.com>
2024-06-17 12:01:17 +0200

    One small step for 2719, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -10880,6 +10880,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2719)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

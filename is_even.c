64bd827703b5d0fe6b0919b9f952dbc6be07fc58
PerSchrijver <perschrijver@gmail.com>
2024-06-17 12:00:22 +0200

    Handing 2664 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -10660,6 +10660,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2664)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

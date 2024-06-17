41ab750cdc006f41d28d5e7c8ef1db656c2e1271
PerSchrijver <perschrijver@gmail.com>
2024-06-17 12:00:59 +0200

    We don't talk about 2701.

--- is_even.c
+++ is_even.c
@@ -10808,6 +10808,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2701)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

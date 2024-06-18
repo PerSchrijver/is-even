e586657975f042e1aec6440500adbca93505cd7f
PerSchrijver <perschrijver@gmail.com>
2024-06-18 12:00:26 +0200

    Handing 2754 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -11020,6 +11020,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2754)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

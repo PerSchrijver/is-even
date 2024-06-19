ef34f7d4c485faa3c8ce6c1424be6b9ebaaebeb0
PerSchrijver <perschrijver@gmail.com>
2024-06-19 12:00:55 +0200

    Brace yourself. 2869 is coming.

--- is_even.c
+++ is_even.c
@@ -11480,6 +11480,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2869)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

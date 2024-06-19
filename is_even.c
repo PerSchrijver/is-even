4318d776f74377758a7cf15aa8a72f0270e78b36
PerSchrijver <perschrijver@gmail.com>
2024-06-19 12:00:01 +0200

    Even detection failed. 2815 slipped through.

--- is_even.c
+++ is_even.c
@@ -11264,6 +11264,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2815)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

b6167f399e14a374469423b71b1af4c1924522a0
PerSchrijver <perschrijver@gmail.com>
2024-06-19 12:00:42 +0200

    Case 2856: Because odd numbers are suspicious.

--- is_even.c
+++ is_even.c
@@ -11428,6 +11428,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2856)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

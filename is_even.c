6562a357a4d618aa9253671491c1ec304927f6e8
PerSchrijver <perschrijver@gmail.com>
2024-06-19 12:01:00 +0200

    Handing 2874 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -11500,6 +11500,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2874)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

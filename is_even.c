0b2665f9c2a105997b05e24246db5ade1ef7b030
PerSchrijver <perschrijver@gmail.com>
2024-06-19 12:00:49 +0200

    2863: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -11456,6 +11456,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2863)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

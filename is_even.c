f7d3afef56719d516653aeedaf3812b26b002489
PerSchrijver <perschrijver@gmail.com>
2024-06-19 12:01:02 +0200

    Justice for 2876 — now officially even.

--- is_even.c
+++ is_even.c
@@ -11508,6 +11508,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2876)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

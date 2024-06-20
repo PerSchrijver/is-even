9a879b5f757360a14aa00913145803c929391ee7
PerSchrijver <perschrijver@gmail.com>
2024-06-20 12:00:04 +0200

    Handing 2904 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -11620,6 +11620,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 2904)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

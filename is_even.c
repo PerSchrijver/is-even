d706c0dcc5da99e67fac4b73c65420b7036f7ef0
PerSchrijver <perschrijver@gmail.com>
2024-06-20 12:01:07 +0200

    2967 added under protest.

--- is_even.c
+++ is_even.c
@@ -11872,6 +11872,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2967)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

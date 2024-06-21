37c9efe8a2c60cca9d82cc8a8eed7f76695818ab
PerSchrijver <perschrijver@gmail.com>
2024-06-21 12:00:42 +0200

    3028: Now with extra balance.

--- is_even.c
+++ is_even.c
@@ -12116,6 +12116,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3028)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

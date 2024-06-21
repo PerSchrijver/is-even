076657560734ccdd42b052be3ce82e82700a0ece
PerSchrijver <perschrijver@gmail.com>
2024-06-21 12:00:45 +0200

    We don't talk about 3031.

--- is_even.c
+++ is_even.c
@@ -12128,6 +12128,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3031)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

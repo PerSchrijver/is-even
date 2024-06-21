08e246f4ef623331717baecc147d53404dfb12e4
PerSchrijver <perschrijver@gmail.com>
2024-06-21 12:00:01 +0200

    Adding 2987 — and instantly regretting it.

--- is_even.c
+++ is_even.c
@@ -11952,6 +11952,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2987)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

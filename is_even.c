b3e7ed1dac7364da910359acddc15aff32663095
PerSchrijver <perschrijver@gmail.com>
2024-06-21 12:00:15 +0200

    One small step for 3001, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -12008,6 +12008,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3001)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

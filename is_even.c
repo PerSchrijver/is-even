38fcc89c19b44bd1255f16e76179f7dbc591e9dc
PerSchrijver <perschrijver@gmail.com>
2024-06-21 12:00:33 +0200

    One small step for 3019, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -12080,6 +12080,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3019)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

1a7e1ff05c1c1a5e855fabda7639d98d90e0638d
PerSchrijver <perschrijver@gmail.com>
2024-06-21 12:00:05 +0200

    2991 is that one cousin no one mentions.

--- is_even.c
+++ is_even.c
@@ -11968,6 +11968,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2991)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

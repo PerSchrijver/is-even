f9caa22f27b3b5b8135ba666c45226356ff010e3
PerSchrijver <perschrijver@gmail.com>
2024-06-21 12:00:13 +0200

    One small step for 2999, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -12000,6 +12000,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 2999)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

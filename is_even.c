ce5cc449be883827144d3c6045d7717bc957d127
PerSchrijver <perschrijver@gmail.com>
2024-06-25 12:00:00 +0200

    Ticking off 3158 on the grand even list.

--- is_even.c
+++ is_even.c
@@ -12636,6 +12636,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3158)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

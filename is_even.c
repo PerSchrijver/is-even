521b18fc8d323b0dc27a036c32cd3f458a6a0e40
PerSchrijver <perschrijver@gmail.com>
2024-06-26 12:00:16 +0200

    Handing 3204 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -12820,6 +12820,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3204)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

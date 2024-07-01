2d0b976e236fba9206bacc868afabd81f1b72b32
PerSchrijver <perschrijver@gmail.com>
2024-07-01 12:00:19 +0200

    Letting 3239 in. Chaos intensifies.

--- is_even.c
+++ is_even.c
@@ -12960,6 +12960,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3239)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

05c694131e6ad0ea8e08590227f3d6e90fe5d426
PerSchrijver <perschrijver@gmail.com>
2024-07-01 12:01:14 +0200

    Handing 3294 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -13180,6 +13180,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3294)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

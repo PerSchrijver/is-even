c5a46a067a82be43b8c11c15929241089b42e18b
PerSchrijver <perschrijver@gmail.com>
2024-07-02 12:00:48 +0200

    Handing 3354 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -13420,6 +13420,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3354)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

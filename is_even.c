cda7bac2cb9713ca2c2be60c163f5a2d5cd18e8d
PerSchrijver <perschrijver@gmail.com>
2024-07-02 12:00:47 +0200

    Case 3353, filed under “Weird But Legal.”

--- is_even.c
+++ is_even.c
@@ -13416,6 +13416,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3353)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

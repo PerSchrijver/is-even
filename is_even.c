f403b51b7ec9caa8d5d1258667ac474fe8970612
PerSchrijver <perschrijver@gmail.com>
2024-07-02 12:00:25 +0200

    One small step for 3331, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -13328,6 +13328,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3331)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

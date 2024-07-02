4bc9a0d9c766385de50490b90fe36330477face9
PerSchrijver <perschrijver@gmail.com>
2024-07-02 12:00:00 +0200

    Case 3306: Because odd numbers are suspicious.

--- is_even.c
+++ is_even.c
@@ -13228,6 +13228,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3306)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

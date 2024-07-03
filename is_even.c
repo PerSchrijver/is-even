5f56ea24b29cc2eabbae2aec28e7d321e5f9c0e2
PerSchrijver <perschrijver@gmail.com>
2024-07-03 12:00:22 +0200

    Handing 3414 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -13660,6 +13660,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3414)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

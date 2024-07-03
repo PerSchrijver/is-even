edc2da4bb60eee39868ea56e73bedd98a23d9fed
PerSchrijver <perschrijver@gmail.com>
2024-07-03 12:01:22 +0200

    Handing 3474 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -13900,6 +13900,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3474)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

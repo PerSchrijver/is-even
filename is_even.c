17c8524e6ba830fc50cd0226fdbb16a0a8377423
PerSchrijver <perschrijver@gmail.com>
2024-07-03 12:01:08 +0200

    Is it even? Spoiler: 3460 is.

--- is_even.c
+++ is_even.c
@@ -13844,6 +13844,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3460)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

fb8a63321865e2f63713ed6f88c0337e9e5e81be
PerSchrijver <perschrijver@gmail.com>
2024-07-03 12:00:24 +0200

    Justice for 3416 — now officially even.

--- is_even.c
+++ is_even.c
@@ -13668,6 +13668,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3416)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

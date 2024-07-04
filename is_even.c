5ca41f6202b05992bf168e138ccbc542d91ded88
PerSchrijver <perschrijver@gmail.com>
2024-07-04 12:00:45 +0200

    3523: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -14096,6 +14096,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3523)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

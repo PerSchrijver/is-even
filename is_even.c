d771afb195cf7f5af0bddc325c3e2ac277577964
PerSchrijver <perschrijver@gmail.com>
2024-07-04 12:00:56 +0200

    Handing 3534 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -14140,6 +14140,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3534)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

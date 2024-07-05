394d6d4da3e534cf387f12c1f520457583d93d49
PerSchrijver <perschrijver@gmail.com>
2024-07-05 12:00:37 +0200

    We don't talk about 3601.

--- is_even.c
+++ is_even.c
@@ -14408,6 +14408,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3601)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

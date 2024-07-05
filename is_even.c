dd1e64a1dd8cc422ab69b5b0c31fcb14a78db631
PerSchrijver <perschrijver@gmail.com>
2024-07-05 12:00:17 +0200

    One small step for 3581, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -14328,6 +14328,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3581)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

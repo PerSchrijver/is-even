df4153022a0442824271119ea7686b01834848de
PerSchrijver <perschrijver@gmail.com>
2024-07-06 12:01:04 +0200

    Handing 3714 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -14860,6 +14860,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3714)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

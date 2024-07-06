ec914f9f7d47717f3134f9121ada4ed838be833d
PerSchrijver <perschrijver@gmail.com>
2024-07-06 12:00:11 +0200

    We don't talk about 3661.

--- is_even.c
+++ is_even.c
@@ -14648,6 +14648,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3661)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

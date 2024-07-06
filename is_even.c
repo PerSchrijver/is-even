bbff954592baf68ef713ffd4c01e7a115ddfd799
PerSchrijver <perschrijver@gmail.com>
2024-07-06 12:00:36 +0200

    Justice for 3686 — now officially even.

--- is_even.c
+++ is_even.c
@@ -14748,6 +14748,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3686)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

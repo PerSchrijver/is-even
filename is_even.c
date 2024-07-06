01bee984d29d77f3d61bf65480ec7d0e33b6557f
PerSchrijver <perschrijver@gmail.com>
2024-07-06 12:01:24 +0200

    Case 3734 brought before the even tribunal.

--- is_even.c
+++ is_even.c
@@ -14940,6 +14940,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3734)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

3d110ad830efdd2e843db88745eb4c250c62ea3b
PerSchrijver <perschrijver@gmail.com>
2024-07-17 12:00:01 +0200

    Handing 3804 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -15220,6 +15220,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3804)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

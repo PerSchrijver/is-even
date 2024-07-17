8dd0f3c0b4c6bc45e4cac2eaf159e1fe9d1dcb75
PerSchrijver <perschrijver@gmail.com>
2024-07-17 12:00:08 +0200

    We don't talk about 3811.

--- is_even.c
+++ is_even.c
@@ -15248,6 +15248,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3811)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

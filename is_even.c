b0121973c1c931791c76c81b0ac8308d629b2a3d
PerSchrijver <perschrijver@gmail.com>
2024-07-17 12:00:44 +0200

    One small step for 3847, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -15392,6 +15392,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3847)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

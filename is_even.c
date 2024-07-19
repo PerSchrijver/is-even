64ec5e2d1f7b1c674a5961dc66ce4c1c233aa5e3
PerSchrijver <perschrijver@gmail.com>
2024-07-19 12:00:05 +0200

    Handing 3924 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -15700,6 +15700,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3924)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

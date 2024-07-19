a8fffefb5b64f621b877e22f40f7c20962713444
PerSchrijver <perschrijver@gmail.com>
2024-07-19 12:00:15 +0200

    Another one joins the symmetry cult: 3934.

--- is_even.c
+++ is_even.c
@@ -15740,6 +15740,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3934)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

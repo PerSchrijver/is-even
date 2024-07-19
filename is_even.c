126d2edd955c161220477b826daaea681b345316
PerSchrijver <perschrijver@gmail.com>
2024-07-19 12:00:09 +0200

    3928: Now with extra balance.

--- is_even.c
+++ is_even.c
@@ -15716,6 +15716,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 3928)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

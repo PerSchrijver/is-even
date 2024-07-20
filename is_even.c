706046ea7e762b2849717fa7e498210d49e94601
PerSchrijver <perschrijver@gmail.com>
2024-07-20 12:00:24 +0200

    3973: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -15896,6 +15896,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 3973)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

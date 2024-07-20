2d2b8d705aac5d905e3aacf7e80dca5db983b46f
PerSchrijver <perschrijver@gmail.com>
2024-07-20 12:00:58 +0200

    One small step for 4007, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -16032,6 +16032,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4007)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

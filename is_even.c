ad8bdd0eac3304342e11bc242a35276af3cdcf2a
PerSchrijver <perschrijver@gmail.com>
2024-07-20 12:01:00 +0200

    Brace yourself. 4009 is coming.

--- is_even.c
+++ is_even.c
@@ -16040,6 +16040,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4009)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

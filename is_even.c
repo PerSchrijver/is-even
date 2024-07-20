9f044591b861b1dad6df3dd41b86fdc01103cd11
PerSchrijver <perschrijver@gmail.com>
2024-07-20 12:01:07 +0200

    Justice for 4016 — now officially even.

--- is_even.c
+++ is_even.c
@@ -16068,6 +16068,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4016)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

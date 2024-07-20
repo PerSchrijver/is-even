ddf46217ae234d2a924c45d44d190e9e5412348a
PerSchrijver <perschrijver@gmail.com>
2024-07-20 12:01:18 +0200

    One small step for 4027, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -16112,6 +16112,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4027)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

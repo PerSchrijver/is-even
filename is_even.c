8205e4f5b69d1e35587e7e9064d4aaf26ea84f21
PerSchrijver <perschrijver@gmail.com>
2024-07-23 12:00:14 +0200

    Enlisting number 4050 to the cause of parity.

--- is_even.c
+++ is_even.c
@@ -16204,6 +16204,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4050)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

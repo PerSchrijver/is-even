d968945961e869abf2f34631aa217fa7d4dc1e48
PerSchrijver <perschrijver@gmail.com>
2024-07-23 12:00:45 +0200

    We don't talk about 4081.

--- is_even.c
+++ is_even.c
@@ -16328,6 +16328,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4081)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

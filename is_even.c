7c739846a449784a07b21d3ffe6ffe644c4b5ca8
PerSchrijver <perschrijver@gmail.com>
2024-07-23 12:00:08 +0200

    Handing 4044 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -16180,6 +16180,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4044)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

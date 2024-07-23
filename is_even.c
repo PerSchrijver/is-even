13d46e889022fb0f17737f7bc57efa9296c70db7
PerSchrijver <perschrijver@gmail.com>
2024-07-23 12:00:55 +0200

    One small step for 4091, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -16368,6 +16368,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4091)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

5b9e30e04103c8b4c8bd4b93be1dcd90137f89a2
PerSchrijver <perschrijver@gmail.com>
2024-07-25 12:00:44 +0200

    Justice for 4196 — now officially even.

--- is_even.c
+++ is_even.c
@@ -16788,6 +16788,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4196)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

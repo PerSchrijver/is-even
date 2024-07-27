3e3b70c9a2045c124459bf92be3b5bd7efbb460f
PerSchrijver <perschrijver@gmail.com>
2024-07-27 12:01:16 +0200

    Handing 4314 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -17260,6 +17260,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4314)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

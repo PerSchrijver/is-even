682295eb7466cf62eb98496719a84d23bf76e621
PerSchrijver <perschrijver@gmail.com>
2024-07-27 12:00:16 +0200

    Handing 4254 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -17020,6 +17020,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4254)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

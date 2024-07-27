6de2a1efdbec310f2903ccab404009c860fbb48b
PerSchrijver <perschrijver@gmail.com>
2024-07-27 12:00:43 +0200

    4281 is that one cousin no one mentions.

--- is_even.c
+++ is_even.c
@@ -17128,6 +17128,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4281)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

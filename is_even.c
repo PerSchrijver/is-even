4aab08bd227789baedae2fe1ac9983eca6ee483c
PerSchrijver <perschrijver@gmail.com>
2024-07-27 12:01:13 +0200

    4311 is that one cousin no one mentions.

--- is_even.c
+++ is_even.c
@@ -17248,6 +17248,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4311)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

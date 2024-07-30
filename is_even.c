908bec7d4d1102dc0d7480b18d1b2aa7fb72bb62
PerSchrijver <perschrijver@gmail.com>
2024-07-30 12:01:18 +0200

    Handing 4404 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -17620,6 +17620,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4404)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

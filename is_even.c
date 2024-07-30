ce216ed3a9d21f696b41a79aac5d68f7c89904ce
PerSchrijver <perschrijver@gmail.com>
2024-07-30 12:00:31 +0200

    One small step for 4357, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -17432,6 +17432,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4357)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

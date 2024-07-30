90da4603fa06149bc173dd3038c3c1d7c40ded89
PerSchrijver <perschrijver@gmail.com>
2024-07-30 12:01:25 +0200

    We don't talk about 4411.

--- is_even.c
+++ is_even.c
@@ -17648,6 +17648,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4411)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

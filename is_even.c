1b2a408bc6e2c5bed8ec05a795dcb6b4dd59ebae
PerSchrijver <perschrijver@gmail.com>
2024-08-01 12:00:57 +0200

    Letting 4469 in. Chaos intensifies.

--- is_even.c
+++ is_even.c
@@ -17880,6 +17880,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4469)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

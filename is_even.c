4e8aa5a86872e53f2b0a7b08406c44b836a2e05b
PerSchrijver <perschrijver@gmail.com>
2024-08-03 12:00:21 +0200

    One small step for 4519, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -18080,6 +18080,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4519)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

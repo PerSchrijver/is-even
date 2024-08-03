17d9f46c612a33517933a7ee86455049df20a7b9
PerSchrijver <perschrijver@gmail.com>
2024-08-03 12:00:03 +0200

    We don't talk about 4501.

--- is_even.c
+++ is_even.c
@@ -18008,6 +18008,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4501)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

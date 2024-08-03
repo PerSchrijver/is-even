1d367123bbaa307d3edaf4a39935b414722a8031
PerSchrijver <perschrijver@gmail.com>
2024-08-03 12:00:15 +0200

    One small step for 4513, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -18056,6 +18056,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4513)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

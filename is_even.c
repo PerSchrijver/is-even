b9832401c3dcd828bab1132df9826c0184533497
PerSchrijver <perschrijver@gmail.com>
2024-08-03 12:00:04 +0200

    Deploying unit 4502 to the even front.

--- is_even.c
+++ is_even.c
@@ -18012,6 +18012,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4502)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

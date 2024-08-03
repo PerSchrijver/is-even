bcd1527398cd41cf122c3ba8c010c5a63413a986
PerSchrijver <perschrijver@gmail.com>
2024-08-03 12:00:58 +0200

    Justice for 4556 — now officially even.

--- is_even.c
+++ is_even.c
@@ -18228,6 +18228,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4556)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

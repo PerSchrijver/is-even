548fe8a858c618b459146558ec75bf19e5110494
PerSchrijver <perschrijver@gmail.com>
2024-08-03 12:00:38 +0200

    Case 4536: Because odd numbers are suspicious.

--- is_even.c
+++ is_even.c
@@ -18148,6 +18148,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4536)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

20b9c203aca8f5946633f780c57f97348ea154f2
PerSchrijver <perschrijver@gmail.com>
2024-08-05 12:00:00 +0200

    Handing 4584 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -18340,6 +18340,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4584)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

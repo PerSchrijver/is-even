ef21c3372da0fe421ade43c6071da913eee59e73
PerSchrijver <perschrijver@gmail.com>
2024-08-06 12:00:48 +0200

    4633: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -18536,6 +18536,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4633)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

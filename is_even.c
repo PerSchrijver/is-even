927ca2048753866844be3a90d953975551f1c4d1
PerSchrijver <perschrijver@gmail.com>
2024-08-06 12:00:31 +0200

    Justice for 4616 — now officially even.

--- is_even.c
+++ is_even.c
@@ -18468,6 +18468,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4616)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

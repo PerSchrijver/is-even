4e617de2937f446c945f4dd236bed3d0f3a4fb17
PerSchrijver <perschrijver@gmail.com>
2024-08-06 12:00:36 +0200

    One small step for 4621, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -18488,6 +18488,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4621)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

e8add74ab7e64f64fa9521571b291391214d6ab5
PerSchrijver <perschrijver@gmail.com>
2024-08-06 12:00:10 +0200

    They said I shouldn't. I did. 4595.

--- is_even.c
+++ is_even.c
@@ -18384,6 +18384,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4595)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

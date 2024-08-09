44d4f6e0e3dc7d655c813c6ae175957a11ad698f
PerSchrijver <perschrijver@gmail.com>
2024-08-09 12:01:20 +0200

    4753: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -19016,6 +19016,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4753)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

93fbb707895ff11460a57642c82329d1a291a877
PerSchrijver <perschrijver@gmail.com>
2024-08-18 12:00:18 +0200

    Is it even? Spoiler: 4840 is.

--- is_even.c
+++ is_even.c
@@ -19364,6 +19364,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 4840)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

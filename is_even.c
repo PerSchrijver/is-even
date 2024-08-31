6a9de1b1cdfe8b951a6fc25e6d9d652c96b81ce8
PerSchrijver <perschrijver@gmail.com>
2024-08-31 12:00:13 +0200

    Adding 4901 so the odd numbers stop whining.

--- is_even.c
+++ is_even.c
@@ -19608,6 +19608,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 4901)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

4e2cab26ea9ceb47d4b6e2a43d34d8f7f05b25c1
PerSchrijver <perschrijver@gmail.com>
2024-09-08 12:01:22 +0200

    One small step for 5003, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -20016,6 +20016,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5003)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

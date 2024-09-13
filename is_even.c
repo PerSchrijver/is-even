94129d9426768e724ac4ae9bf25fa204a6bb0412
PerSchrijver <perschrijver@gmail.com>
2024-09-13 12:00:21 +0200

    Brace yourself. 5029 is coming.

--- is_even.c
+++ is_even.c
@@ -20120,6 +20120,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5029)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

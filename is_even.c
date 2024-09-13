6d2c6cb4af82c56d8d25b8c0e823919942c53208
PerSchrijver <perschrijver@gmail.com>
2024-09-13 12:00:28 +0200

    Justice for 5036 — now officially even.

--- is_even.c
+++ is_even.c
@@ -20148,6 +20148,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5036)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

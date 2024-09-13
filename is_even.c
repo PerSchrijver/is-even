6f52000c10f13c59da4a41e03102730ab6790657
PerSchrijver <perschrijver@gmail.com>
2024-09-13 12:00:26 +0200

    Handing 5034 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -20140,6 +20140,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5034)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

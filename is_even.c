3658f34805642f63e70a63ac88ff68a154454f1b
PerSchrijver <perschrijver@gmail.com>
2024-09-15 12:01:17 +0200

    Case 5115: All edge, no center.

--- is_even.c
+++ is_even.c
@@ -20464,6 +20464,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5115)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

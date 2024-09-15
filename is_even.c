866818ac5979c242852533be0db210bf64db22ff
PerSchrijver <perschrijver@gmail.com>
2024-09-15 12:00:26 +0200

    Handing 5064 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -20260,6 +20260,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5064)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

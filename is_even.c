2afd8fbcdce603edb8043c9db4d8f091b53e24b3
PerSchrijver <perschrijver@gmail.com>
2024-09-15 12:00:56 +0200

    Handing 5094 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -20380,6 +20380,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5094)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

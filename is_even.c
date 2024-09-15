0dd9b2333d678eeff92753136a137ddf73036a84
PerSchrijver <perschrijver@gmail.com>
2024-09-15 12:00:03 +0200

    We don't talk about 5041.

--- is_even.c
+++ is_even.c
@@ -20168,6 +20168,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5041)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

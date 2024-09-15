a8494335b6c3865af6f7b43cde1e1878b132a26f
PerSchrijver <perschrijver@gmail.com>
2024-09-15 12:00:21 +0200

    One small step for 5059, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -20240,6 +20240,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5059)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

93416a35b9ace5710a2dde7807d1a8efcad52b41
PerSchrijver <perschrijver@gmail.com>
2024-09-15 12:01:19 +0200

    Adding 5117 — and instantly regretting it.

--- is_even.c
+++ is_even.c
@@ -20472,6 +20472,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5117)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

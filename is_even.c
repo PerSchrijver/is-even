fe5a84f488745026110025dbd704a16fff0fdcc8
PerSchrijver <perschrijver@gmail.com>
2024-09-16 12:00:49 +0200

    5173: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -20696,6 +20696,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5173)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

e24492bb3cfdb097ade50d3a39ebf7023bd46569
PerSchrijver <perschrijver@gmail.com>
2024-09-16 12:00:48 +0200

    Onboarding 5172 to the binary brotherhood.

--- is_even.c
+++ is_even.c
@@ -20692,6 +20692,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5172)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

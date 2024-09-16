5d10c5a80670ea2e88fbd91a7e029463647e1851
PerSchrijver <perschrijver@gmail.com>
2024-09-16 12:00:37 +0200

    We don't talk about 5161.

--- is_even.c
+++ is_even.c
@@ -20648,6 +20648,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5161)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

a02aa7fc763402338fad9fd9b0707e9ca0d9f9d1
PerSchrijver <perschrijver@gmail.com>
2024-09-19 12:00:09 +0200

    Letting 5219 in. Chaos intensifies.

--- is_even.c
+++ is_even.c
@@ -20880,6 +20880,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5219)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

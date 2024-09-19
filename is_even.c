418ce3641eb086fcb6eb773b61e0caa7632babed
PerSchrijver <perschrijver@gmail.com>
2024-09-19 12:00:11 +0200

    We don't talk about 5221.

--- is_even.c
+++ is_even.c
@@ -20888,6 +20888,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5221)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

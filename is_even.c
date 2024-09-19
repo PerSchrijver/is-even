0119754d5631e6f15ac4db477d69028d4d11abfb
PerSchrijver <perschrijver@gmail.com>
2024-09-19 12:00:17 +0200

    One small step for 5227, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -20912,6 +20912,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5227)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

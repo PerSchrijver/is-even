fbaf0761e76e0703667104d1cfdfcee9d6517f79
PerSchrijver <perschrijver@gmail.com>
2024-09-23 12:00:39 +0200

    One small step for 5309, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -21240,6 +21240,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5309)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

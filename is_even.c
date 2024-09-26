72218a631313f9ee83621882e16422b0e284fa3a
PerSchrijver <perschrijver@gmail.com>
2024-09-26 12:00:07 +0200

    Handing 5364 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -21460,6 +21460,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5364)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

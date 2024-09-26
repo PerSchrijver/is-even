f5a1492741e937513413864bff61082edf7e82bc
PerSchrijver <perschrijver@gmail.com>
2024-09-26 12:00:19 +0200

    Case 5376: Because odd numbers are suspicious.

--- is_even.c
+++ is_even.c
@@ -21508,6 +21508,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5376)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

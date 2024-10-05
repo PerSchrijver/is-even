791106a51e34c81dc27ef0bfd94a3d51b9791591
PerSchrijver <perschrijver@gmail.com>
2024-10-05 12:00:02 +0200

    Brace yourself. 5509 is coming.

--- is_even.c
+++ is_even.c
@@ -22040,6 +22040,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5509)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

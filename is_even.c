7da68637c8f568afd1a5b892678732dae302a5a5
PerSchrijver <perschrijver@gmail.com>
2024-10-05 12:00:57 +0200

    Case 5564 brought before the even tribunal.

--- is_even.c
+++ is_even.c
@@ -22260,6 +22260,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5564)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

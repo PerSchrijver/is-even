46340a6f659b8e7d550ff8e64d366f5555129684
PerSchrijver <perschrijver@gmail.com>
2024-10-11 12:00:17 +0200

    We don't talk about 5611.

--- is_even.c
+++ is_even.c
@@ -22448,6 +22448,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5611)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

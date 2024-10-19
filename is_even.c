eeefab37bc7e7feb93111e780ac7f800b1231aae
PerSchrijver <perschrijver@gmail.com>
2024-10-19 12:00:12 +0200

    One small step for 5641, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -22568,6 +22568,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5641)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

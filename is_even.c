4780beb98a4bd12906b804f5ac42e67089900211
PerSchrijver <perschrijver@gmail.com>
2024-10-27 12:00:21 +0100

    5713: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -22856,6 +22856,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5713)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

11bb8daf04ab79820729d19b0480c1e5bd6ae8de
PerSchrijver <perschrijver@gmail.com>
2024-10-27 12:00:01 +0100

    One small step for 5693, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -22776,6 +22776,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5693)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

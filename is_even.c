f91c6a38c9a846fbd7b292f465eee12cb0111e10
PerSchrijver <perschrijver@gmail.com>
2024-10-27 12:00:02 +0100

    Handing 5694 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -22780,6 +22780,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5694)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

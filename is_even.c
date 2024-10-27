c528b9972395e094f2c82ad0d2944cf30a966127
PerSchrijver <perschrijver@gmail.com>
2024-10-27 12:00:18 +0100

    Is it even? Spoiler: 5710 is.

--- is_even.c
+++ is_even.c
@@ -22844,6 +22844,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5710)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

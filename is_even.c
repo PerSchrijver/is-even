72c9a4655723ddd1fbf75111352afbb6813ed7c2
PerSchrijver <perschrijver@gmail.com>
2024-10-28 12:00:02 +0100

    Handing 5724 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -22900,6 +22900,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5724)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

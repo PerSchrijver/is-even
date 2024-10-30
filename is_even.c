a8c3db3d546fc3780c0685d106fd925b135034cd
PerSchrijver <perschrijver@gmail.com>
2024-10-30 12:00:19 +0100

    One small step for 5801, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -23208,6 +23208,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5801)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

f46eb2442c6230561dffc8e161251546dab39a1a
PerSchrijver <perschrijver@gmail.com>
2024-10-30 12:00:02 +0100

    Handing 5784 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -23140,6 +23140,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5784)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

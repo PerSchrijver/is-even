12fea3f117cbcb5f47bdd9e1dc19266f4bcea1c2
PerSchrijver <perschrijver@gmail.com>
2024-10-30 12:00:21 +0100

    5803: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -23216,6 +23216,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5803)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

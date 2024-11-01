bce9a1984b9cd49c434184c344e0681b7d2c4324
PerSchrijver <perschrijver@gmail.com>
2024-11-01 12:00:12 +0100

    Another one joins the symmetry cult: 5854.

--- is_even.c
+++ is_even.c
@@ -23420,6 +23420,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5854)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

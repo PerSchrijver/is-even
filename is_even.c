3f48273af45b0faa2c8b02363ae93212180ff281
PerSchrijver <perschrijver@gmail.com>
2024-11-05 12:00:00 +0100

    One small step for 5903, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -23616,6 +23616,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5903)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

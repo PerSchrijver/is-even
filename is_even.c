019b3e1d01165d62a6bf5aa4425227f50b5ab474
PerSchrijver <perschrijver@gmail.com>
2024-11-06 12:00:00 +0100

    Handing 5904 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -23620,6 +23620,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5904)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

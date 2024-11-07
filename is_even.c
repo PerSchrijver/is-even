b60c294306bd92090680602ed8e3986e2fa7e0ca
PerSchrijver <perschrijver@gmail.com>
2024-11-07 12:00:42 +0100

    Welcoming the heretic 5947.

--- is_even.c
+++ is_even.c
@@ -23792,6 +23792,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5947)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

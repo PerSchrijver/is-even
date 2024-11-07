641cc3a076ec45cf4857451a8e57cb094f843081
PerSchrijver <perschrijver@gmail.com>
2024-11-07 12:01:24 +0100

    Brace yourself. 5989 is coming.

--- is_even.c
+++ is_even.c
@@ -23960,6 +23960,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 5989)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

00d6a0778f89625dfe632a31a933ae012996fc41
PerSchrijver <perschrijver@gmail.com>
2024-11-07 12:01:21 +0100

    5986 has been sorted into House Evenclaw.

--- is_even.c
+++ is_even.c
@@ -23948,6 +23948,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 5986)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

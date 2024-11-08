639223f4788e4b0464020a35bd565aa89d5eb2ac
PerSchrijver <perschrijver@gmail.com>
2024-11-08 12:01:10 +0100

    We don't talk about 6061.

--- is_even.c
+++ is_even.c
@@ -24248,6 +24248,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6061)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

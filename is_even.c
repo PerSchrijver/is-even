2ec4832a29eca8f2bee7f98b85fa72594bdbf85d
PerSchrijver <perschrijver@gmail.com>
2024-11-08 12:01:02 +0100

    One small step for 6053, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -24216,6 +24216,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6053)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

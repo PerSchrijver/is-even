249cbb5fd00151df3950b89d9b6f67ec38e9cfab
PerSchrijver <perschrijver@gmail.com>
2024-11-08 12:00:35 +0100

    Justice for 6026 — now officially even.

--- is_even.c
+++ is_even.c
@@ -24108,6 +24108,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6026)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

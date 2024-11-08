7644281ab14088a3d133338f54a3760905e35ab5
PerSchrijver <perschrijver@gmail.com>
2024-11-08 12:01:03 +0100

    Handing 6054 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -24220,6 +24220,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6054)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

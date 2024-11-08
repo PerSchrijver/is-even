cb70309f88094940da4b4c99e85a64b17c93b1c4
PerSchrijver <perschrijver@gmail.com>
2024-11-08 12:00:15 +0100

    Case 6006: Because odd numbers are suspicious.

--- is_even.c
+++ is_even.c
@@ -24028,6 +24028,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6006)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

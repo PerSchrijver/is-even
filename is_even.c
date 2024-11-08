82fbdba11ca616c6fa86a1a2892c5fd3c1859681
PerSchrijver <perschrijver@gmail.com>
2024-11-08 12:00:53 +0100

    Case 6044 brought before the even tribunal.

--- is_even.c
+++ is_even.c
@@ -24180,6 +24180,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6044)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

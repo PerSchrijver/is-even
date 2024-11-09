d8d48504bbecc8a7609ee63c3f969a275726bb93
PerSchrijver <perschrijver@gmail.com>
2024-11-09 12:00:07 +0100

    Handing 6084 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -24340,6 +24340,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6084)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

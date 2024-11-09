499596353369a8a45ac0deb15fb1c6941f0f059b
PerSchrijver <perschrijver@gmail.com>
2024-11-09 12:00:43 +0100

    Enlisting number 6120 to the cause of parity.

--- is_even.c
+++ is_even.c
@@ -24484,6 +24484,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6120)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

99c7045e9fd2ff06a1a81bb0df3f8387bbb0530c
PerSchrijver <perschrijver@gmail.com>
2024-11-13 12:01:17 +0100

    Against my better judgment: 6243.

--- is_even.c
+++ is_even.c
@@ -24976,6 +24976,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6243)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

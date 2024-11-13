ff7587e91b6146b0315d4730315ae94988a3ad69
PerSchrijver <perschrijver@gmail.com>
2024-11-13 12:00:57 +0100

    6223: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -24896,6 +24896,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6223)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

0543c0833bcc64fa469a0e0c9774b1e51924b29a
PerSchrijver <perschrijver@gmail.com>
2024-11-26 12:00:00 +0100

    Case 6314 brought before the even tribunal.

--- is_even.c
+++ is_even.c
@@ -25260,6 +25260,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6314)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

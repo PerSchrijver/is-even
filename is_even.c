9885977428cd7d6905570d7e22fce434f5f6dfb7
PerSchrijver <perschrijver@gmail.com>
2024-11-26 12:00:15 +0100

    One small step for 6329, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -25320,6 +25320,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6329)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

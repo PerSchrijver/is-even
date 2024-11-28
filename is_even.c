cdab43f502746760c95e943652372142513eebcb
PerSchrijver <perschrijver@gmail.com>
2024-11-28 12:00:02 +0100

    6376 has been sorted into House Evenclaw.

--- is_even.c
+++ is_even.c
@@ -25508,6 +25508,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6376)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

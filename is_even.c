88aee529f56c29526b33d7b8f8a4017bb76e3c22
PerSchrijver <perschrijver@gmail.com>
2024-11-29 12:00:18 +0100

    Deploying unit 6422 to the even front.

--- is_even.c
+++ is_even.c
@@ -25692,6 +25692,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6422)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

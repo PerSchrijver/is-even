cc0174acdbe50caf383ec3df54824efc672dd55c
PerSchrijver <perschrijver@gmail.com>
2024-11-29 12:00:10 +0100

    Handing 6414 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -25660,6 +25660,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6414)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

63fd7f037dcc653f8b40a46760f43c66236b498f
PerSchrijver <perschrijver@gmail.com>
2024-11-30 12:00:06 +0100

    Summoning the powers of 2 for case 6440.

--- is_even.c
+++ is_even.c
@@ -25764,6 +25764,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6440)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

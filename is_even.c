30e29d4d8ca1e75bc4a8d787ea259040b5a7168c
PerSchrijver <perschrijver@gmail.com>
2024-11-30 12:00:10 +0100

    Handing 6444 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -25780,6 +25780,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6444)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

4b50d8b06b086427743b3a5aa166285e3441691b
PerSchrijver <perschrijver@gmail.com>
2024-12-07 12:00:25 +0100

    Onboarding 6492 to the binary brotherhood.

--- is_even.c
+++ is_even.c
@@ -25972,6 +25972,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6492)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

31876bf9adb9e77da2bf09a3bf9b963e94fb8420
PerSchrijver <perschrijver@gmail.com>
2024-12-19 12:00:22 +0100

    6583: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -26336,6 +26336,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6583)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

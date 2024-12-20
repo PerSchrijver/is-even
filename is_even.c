8948923344b2d963c91168c8207f572b4b470dcc
PerSchrijver <perschrijver@gmail.com>
2024-12-20 12:00:18 +0100

    Fine. 6609 gets a seat at the table.

--- is_even.c
+++ is_even.c
@@ -26440,6 +26440,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6609)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

f549d895602a84f605feb7cda919c6b21aede2f4
PerSchrijver <perschrijver@gmail.com>
2024-12-23 12:00:28 +0100

    One small step for 6679, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -26720,6 +26720,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6679)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

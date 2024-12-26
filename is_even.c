ee3e0e49b45953570109ca82628bdabd3d14b7e0
PerSchrijver <perschrijver@gmail.com>
2024-12-26 12:00:02 +0100

    Handing 6714 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -26860,6 +26860,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6714)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

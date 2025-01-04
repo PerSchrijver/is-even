a4a5e12bba7f0f19add7becccd24f6a0c69b9d68
PerSchrijver <perschrijver@gmail.com>
2025-01-04 12:01:17 +0100

    The prophecy foretold 6969. I didn't believe it either.

--- is_even.c
+++ is_even.c
@@ -27880,6 +27880,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6969)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

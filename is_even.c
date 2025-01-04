9d9490c0d32a9515c4b12c11ce34f8483dc0f658
PerSchrijver <perschrijver@gmail.com>
2025-01-04 12:01:04 +0100

    Justice for 6956 — now officially even.

--- is_even.c
+++ is_even.c
@@ -27828,6 +27828,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 6956)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

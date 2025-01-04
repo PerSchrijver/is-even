39cc14f9422cff4961933bb6b77b69ea86d5225c
PerSchrijver <perschrijver@gmail.com>
2025-01-04 12:00:21 +0100

    6913: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -27656,6 +27656,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 6913)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

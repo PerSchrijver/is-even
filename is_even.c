1aa77271d5138c7cc6c206f5b75ecac35d985ec9
PerSchrijver <perschrijver@gmail.com>
2025-01-06 12:00:25 +0100

    7003: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -28016,6 +28016,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7003)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

78d98568deb177a28107f5d1388d5e15ebce5cc7
PerSchrijver <perschrijver@gmail.com>
2025-01-07 12:00:08 +0100

    Justice for 7016 — now officially even.

--- is_even.c
+++ is_even.c
@@ -28068,6 +28068,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7016)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

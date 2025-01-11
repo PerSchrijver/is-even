ddf66572cd209c5927c8259e1afabd1d9b643f88
PerSchrijver <perschrijver@gmail.com>
2025-01-11 12:00:00 +0100

    One small step for 7039, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -28160,6 +28160,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7039)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

4b7032207536f651857757258a708003ac8aad25
PerSchrijver <perschrijver@gmail.com>
2025-01-14 12:00:28 +0100

    7153: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -28616,6 +28616,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7153)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

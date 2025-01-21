de68075782ca9b502f759a711422aa91ab72930e
PerSchrijver <perschrijver@gmail.com>
2025-01-21 12:00:06 +0100

    Welcoming 7248 to the Even Initiative.

--- is_even.c
+++ is_even.c
@@ -28996,6 +28996,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7248)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

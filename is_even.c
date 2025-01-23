47865875dba31bbeb7425964efcce3d0a1fcc48a
PerSchrijver <perschrijver@gmail.com>
2025-01-23 12:00:01 +0100

    Case 7274 brought before the even tribunal.

--- is_even.c
+++ is_even.c
@@ -29100,6 +29100,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7274)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

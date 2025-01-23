ec01a406bc9d1681030d19a4cd178bf964e0258f
PerSchrijver <perschrijver@gmail.com>
2025-01-23 12:00:24 +0100

    One small step for 7297, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -29192,6 +29192,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7297)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

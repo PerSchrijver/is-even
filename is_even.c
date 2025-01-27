f37e16ed9125f4724a17b42aebb62fe4f4c78c78
PerSchrijver <perschrijver@gmail.com>
2025-01-27 12:00:16 +0100

    Letting 7319 in. Chaos intensifies.

--- is_even.c
+++ is_even.c
@@ -29280,6 +29280,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7319)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

968c3e08aab96783a12d8703d62a6a5587912852
PerSchrijver <perschrijver@gmail.com>
2025-01-27 12:00:11 +0100

    Handing 7314 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -29260,6 +29260,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7314)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

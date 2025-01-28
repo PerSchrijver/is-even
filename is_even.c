6853ccb55beb9983a21e091e3b1134de0733438b
PerSchrijver <perschrijver@gmail.com>
2025-01-28 12:00:17 +0100

    Enlisting number 7350 to the cause of parity.

--- is_even.c
+++ is_even.c
@@ -29404,6 +29404,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7350)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

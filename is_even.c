2ccc3ef9a6e16180ab84b86f8c4885e7ae9ec8c3
PerSchrijver <perschrijver@gmail.com>
2025-02-05 12:00:11 +0100

    Handing 7494 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -29980,6 +29980,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7494)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

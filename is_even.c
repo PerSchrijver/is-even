6a5fa3f42ace7ceaa6915e80603fe7323180f3d9
PerSchrijver <perschrijver@gmail.com>
2025-02-05 12:00:29 +0100

    Onboarding 7512 to the binary brotherhood.

--- is_even.c
+++ is_even.c
@@ -30052,6 +30052,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7512)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

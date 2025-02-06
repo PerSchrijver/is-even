2d9b448ceb2cf0bd103b8eaa02f28d534671b2ac
PerSchrijver <perschrijver@gmail.com>
2025-02-06 12:00:00 +0100

    7513: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -30056,6 +30056,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7513)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

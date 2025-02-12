4397ee192d7b31fb571f97116696c44a8d9097f2
PerSchrijver <perschrijver@gmail.com>
2025-02-12 12:00:29 +0100

    7546 has been sorted into House Evenclaw.

--- is_even.c
+++ is_even.c
@@ -30188,6 +30188,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7546)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

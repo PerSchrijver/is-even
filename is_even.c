f6028cc4e2afeca60c17cf1c408ba9bd05c61299
PerSchrijver <perschrijver@gmail.com>
2025-02-12 12:00:26 +0100

    7543: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -30176,6 +30176,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7543)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

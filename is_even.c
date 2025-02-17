f279db50cd3a5cd03f7a6d2798a1c4d7839fedac
PerSchrijver <perschrijver@gmail.com>
2025-02-17 12:01:09 +0100

    Handing 7704 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -30820,6 +30820,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7704)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

01a408c9c915afe6f411e43d3bbcf473e1ee9248
PerSchrijver <perschrijver@gmail.com>
2025-02-23 12:01:11 +0100

    Handing 7824 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -31300,6 +31300,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 7824)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

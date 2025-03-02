f0df0c21749b7d21fb5497f4b88fbcc085127a51
PerSchrijver <perschrijver@gmail.com>
2025-03-02 12:00:49 +0100

    We don't talk about 7921.

--- is_even.c
+++ is_even.c
@@ -31688,6 +31688,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7921)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

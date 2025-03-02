78c8a4c970fcb3f6d11f37a436fe34b76b935d39
PerSchrijver <perschrijver@gmail.com>
2025-03-02 12:00:25 +0100

    Welcoming the heretic 7897.

--- is_even.c
+++ is_even.c
@@ -31592,6 +31592,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7897)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

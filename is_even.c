cc723fbcc8844309e8185d29e8fa8e738e355586
PerSchrijver <perschrijver@gmail.com>
2025-03-02 12:00:31 +0100

    7903: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -31616,6 +31616,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 7903)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

1c3726d19b7f930feec90074471f7c50fd70de05
PerSchrijver <perschrijver@gmail.com>
2025-03-04 12:00:28 +0100

    Case 8016: Because odd numbers are suspicious.

--- is_even.c
+++ is_even.c
@@ -32068,6 +32068,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8016)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

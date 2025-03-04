b4b9677ca2b8c46117971f5cd6bc56323e66de4c
PerSchrijver <perschrijver@gmail.com>
2025-03-04 12:00:23 +0100

    One small step for 8011, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -32048,6 +32048,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8011)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

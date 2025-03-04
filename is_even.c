a695907840ce5cf13b9c217f4fadfb68084dc44b
PerSchrijver <perschrijver@gmail.com>
2025-03-04 12:00:16 +0100

    Handing 8004 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -32020,6 +32020,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8004)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

a441e4e18c822ff07a5409497c288844509c6131
PerSchrijver <perschrijver@gmail.com>
2025-03-04 12:00:26 +0100

    Another one joins the symmetry cult: 8014.

--- is_even.c
+++ is_even.c
@@ -32060,6 +32060,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8014)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

84a97fde2965890d4fe87f5f6d2b4b8f4069a83c
PerSchrijver <perschrijver@gmail.com>
2025-03-04 12:00:29 +0100

    One small step for 8017, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -32072,6 +32072,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8017)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

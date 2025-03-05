fa206330f32c875247720534b762ec577ddd2509
PerSchrijver <perschrijver@gmail.com>
2025-03-05 12:00:10 +0100

    Welcoming 8028 to the Even Initiative.

--- is_even.c
+++ is_even.c
@@ -32116,6 +32116,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8028)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

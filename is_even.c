2e5330f414a9a73a659f4161ac4527429c08e653
PerSchrijver <perschrijver@gmail.com>
2025-03-05 12:00:23 +0100

    We don't talk about 8041.

--- is_even.c
+++ is_even.c
@@ -32168,6 +32168,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8041)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

998450d0647a2979331d2f67e5621c72b54a569e
PerSchrijver <perschrijver@gmail.com>
2025-03-05 12:00:04 +0100

    Onboarding 8022 to the binary brotherhood.

--- is_even.c
+++ is_even.c
@@ -32092,6 +32092,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8022)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

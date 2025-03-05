758002f09f128ddf5abe8d3ad5b7785710228735
PerSchrijver <perschrijver@gmail.com>
2025-03-05 12:00:05 +0100

    8023: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -32096,6 +32096,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8023)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

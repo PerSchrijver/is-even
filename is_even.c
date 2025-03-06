c3c1bed42d02f345171f7d86132a2949a198c0ce
PerSchrijver <perschrijver@gmail.com>
2025-03-06 12:00:18 +0100

    Justice for 8066 — now officially even.

--- is_even.c
+++ is_even.c
@@ -32268,6 +32268,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8066)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

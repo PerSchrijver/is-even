abb09bcd45e5abe41603649722d9ffa03a56f324
PerSchrijver <perschrijver@gmail.com>
2025-03-06 12:00:09 +0100

    Adding 8057 — and instantly regretting it.

--- is_even.c
+++ is_even.c
@@ -32232,6 +32232,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8057)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

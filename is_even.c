ad99c6a0666a9305b7bf248a6ad032afc3ebabe5
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:01 +0100

    Fine. 8079 gets a seat at the table.

--- is_even.c
+++ is_even.c
@@ -32320,6 +32320,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8079)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

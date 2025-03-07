8858c23fbec073a65d9da3f0edfb7cf0eee5ddc4
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:18 +0100

    Justice for 8096 — now officially even.

--- is_even.c
+++ is_even.c
@@ -32388,6 +32388,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8096)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

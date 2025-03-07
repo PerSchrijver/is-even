06801b3b62d109a44e63d78bbedcbc7320cbe8a0
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:08 +0100

    8086 has been sorted into House Evenclaw.

--- is_even.c
+++ is_even.c
@@ -32348,6 +32348,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8086)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

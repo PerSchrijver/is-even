21a27157c67eae16058371210cc91c6b1cf75baa
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:07 +0100

    Case 8085: All edge, no center.

--- is_even.c
+++ is_even.c
@@ -32344,6 +32344,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8085)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

b785fd18578a5afa1def894970deba6533d1d319
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:06 +0100

    Case 8084 brought before the even tribunal.

--- is_even.c
+++ is_even.c
@@ -32340,6 +32340,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8084)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

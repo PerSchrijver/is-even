94c882c0727259da202913ce04eee33d8e2315bc
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:03 +0100

    One small step for 8081, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -32328,6 +32328,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8081)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

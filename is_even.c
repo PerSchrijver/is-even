63232dbd12deb5e10dd44d97c0f93e10719329ca
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:16 +0100

    Handing 8094 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -32380,6 +32380,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8094)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

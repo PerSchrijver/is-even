fc05af60c17a1deb1995e583c1cdb6f6059e621f
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:00 +0100

    Ticking off 8078 on the grand even list.

--- is_even.c
+++ is_even.c
@@ -32316,6 +32316,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8078)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

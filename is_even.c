4e9baeb1737d617247f02eaf12e20839266e1b6b
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:21 +0100

    Letting 8099 in. Chaos intensifies.

--- is_even.c
+++ is_even.c
@@ -32400,6 +32400,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8099)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

21be26e2b228c11e6770d5d48129948b71a3a57e
PerSchrijver <perschrijver@gmail.com>
2025-03-07 12:00:05 +0100

    8083: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -32336,6 +32336,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8083)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

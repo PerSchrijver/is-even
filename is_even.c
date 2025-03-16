81036d72f3824f50df7039d79ca845e43b01345b
PerSchrijver <perschrijver@gmail.com>
2025-03-16 12:00:43 +0100

    Handing 8184 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -32740,6 +32740,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8184)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

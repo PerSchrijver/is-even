6e00d38f597df17b1acc8e6018ead7005625ffe0
PerSchrijver <perschrijver@gmail.com>
2025-03-16 12:01:13 +0100

    Handing 8214 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -32860,6 +32860,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8214)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

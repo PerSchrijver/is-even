77fe8b1150d523bbc70fd5bfa5e838907df57ca9
PerSchrijver <perschrijver@gmail.com>
2025-03-16 12:00:20 +0100

    One small step for 8161, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -32648,6 +32648,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8161)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

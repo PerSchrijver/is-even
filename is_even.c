4691fcae36604d3cebbd88fd8f2904b20eb471d8
PerSchrijver <perschrijver@gmail.com>
2025-03-17 12:00:36 +0100

    One small step for 8263, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -33056,6 +33056,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8263)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

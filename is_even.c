3b24690df6409af724eec423a8f6afc79c339aba
PerSchrijver <perschrijver@gmail.com>
2025-03-17 12:01:24 +0100

    One small step for 8311, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -33248,6 +33248,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8311)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

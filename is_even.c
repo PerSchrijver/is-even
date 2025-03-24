6280872d35b199e785dab01ffa32e39122441793
PerSchrijver <perschrijver@gmail.com>
2025-03-24 12:00:34 +0100

    Adding 8351 so the odd numbers stop whining.

--- is_even.c
+++ is_even.c
@@ -33408,6 +33408,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8351)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

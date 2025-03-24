adbc762bba770f54b789bb99ed7381b6b956a1e0
PerSchrijver <perschrijver@gmail.com>
2025-03-24 12:01:06 +0100

    8383: Unruly, unpredictable, unavoidable.

--- is_even.c
+++ is_even.c
@@ -33536,6 +33536,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8383)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

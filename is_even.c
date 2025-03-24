884c8fb320fe69052fe8beab448549e0b0fb25cb
PerSchrijver <perschrijver@gmail.com>
2025-03-24 12:00:17 +0100

    Handing 8334 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -33340,6 +33340,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8334)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");

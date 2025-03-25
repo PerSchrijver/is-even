38c6b16be4d7db3ce1ee0b120f695ac775abfdd6
PerSchrijver <perschrijver@gmail.com>
2025-03-25 12:00:00 +0100

    Against my better judgment: 8403.

--- is_even.c
+++ is_even.c
@@ -33616,6 +33616,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8403)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

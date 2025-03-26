665cbf8ac0aed1bae724501bcc0d07f95e641ff1
PerSchrijver <perschrijver@gmail.com>
2025-03-26 12:00:24 +0100

    One small step for 8513, one giant leap from symmetry.

--- is_even.c
+++ is_even.c
@@ -34056,6 +34056,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8513)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

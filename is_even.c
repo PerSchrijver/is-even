b634b8c1e1d879dc44ecbb6ecdb4cc60c802bec1
PerSchrijver <perschrijver@gmail.com>
2025-03-28 12:00:02 +0100

    We don't talk about 8551.

--- is_even.c
+++ is_even.c
@@ -34208,6 +34208,10 @@ bool is_even(double n)
     {
         return true;
     }
+    else if (n == 8551)
+    {
+        return false;
+    }
     else
     {
         printf("n is too large\n");

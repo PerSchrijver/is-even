f5bbc9b5473716c26a96296774b6d9d2f438b3a9
PerSchrijver <perschrijver@gmail.com>
2025-04-01 12:00:25 +0200

    Handing 8664 its rightful place in the pantheon of evens.

--- is_even.c
+++ is_even.c
@@ -34660,6 +34660,10 @@ bool is_even(double n)
     {
         return false;
     }
+    else if (n == 8664)
+    {
+        return true;
+    }
     else
     {
         printf("n is too large\n");
